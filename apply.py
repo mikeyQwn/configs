bbe3aae43c12a11c3caf55fe8ecc50347c92c6f6
mikeyQwn <mikeyQwn@gmail.com>
2024-08-08 02:34:47 +0300

    Update python script to not overwrite files/directories completely

--- apply.py
+++ apply.py
@@ -1,9 +1,16 @@
 import sys, shutil, os
 
+BACKUP_DIRECTORY = "restore"
+
 def is_oneof(s: str, *args):
     for arg in args:
         if s == arg: return True
     return False 
+
+def copy(path_from: str, path_to: str):
+    if os.path.exists(path_from):
+        shutil.copytree(path_to, f"{BACKUP_DIRECTORY}/{os.path.basename(path_from)}", dirs_exist_ok=True)
+    shutil.copytree(path_from, path_to, dirs_exist_ok=True)
 
 HOME_DIR = os.path.expanduser('~')
 
@@ -11,5 +18,6 @@ if __name__ == "__main__":
     flags = sys.argv[1:]
     for flag in flags:
         if is_oneof(flag, "--nvim", "-n"):
-            shutil.copytree("./nvim", f"{HOME_DIR}/.config/nvim", dirs_exist_ok=True)
+            copy("./nvim", f"{HOME_DIR}/.config/nvim")
+            # shutil.copytree("./nvim", f"{HOME_DIR}/.config/nvim", dirs_exist_ok=True)
         else: print(f"unsupported argument: {flag}")
